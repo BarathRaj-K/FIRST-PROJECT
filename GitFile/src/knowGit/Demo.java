736f2c36842d655f8eb9153a4285cac4ddc07b1d
Admin <kbarath15032004@gmail.com>
2022-12-19 16:41:48 +0530

    first commit

---
+++ GitFile/src/knowGit/Demo.java
@@ -0,0 +1,11 @@
+package knowGit;
+public class Demo {
+	public static void main(String[] args) {
+    int age = 10;
+    if(age>=18)
+    	System.out.println("Can vote");
+    else
+    	System.out.println("Cant vote");
+	}
+
+}
